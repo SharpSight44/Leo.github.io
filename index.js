24f635bdbe7fa4da6fb579a10390ec16d960bff5
SharpSight44 <Leoest011@gmail.com>
2022-07-02 14:55:56 +0000

    update 1

--- index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-
-import "./index.css";
-
-ReactDom.render(<Layout/>, document.querySelector("#app"));
--- main.js
+++
@@ -1,8 +0,0 @@
-
-
-import React from "react";
-
-export const Main = () => {
-  return <div > <h1>
-    You Are the ONE!!!!! </h1></div>;
-};
