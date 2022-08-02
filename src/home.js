6a85811b7f6e06e71508794cb0be1a3ff7d7e678
SharpSight44 <Leoest011@gmail.com>
2022-08-02 00:50:16 +0000

    update cards

--- src/home.js
+++ src/home.js
@@ -15,12 +15,14 @@ export default function Home(){
             <div className="About">
                 <div className="cardsHouse">
             <div className="myCards">
-                 <div style={{height:"50%"}} ><img style={{width:"100%", borderRadius:"10px 10px 0px 0px"}}src="https://www.gcu.edu/sites/default/files/2020-09/programming.jpg" /></div>
-                 <div > Text Information Goes there for me if i want to do text</div>
+                
+                 <div > <img style={{height:"100%", opacity:".7"}} src="https://www.gcu.edu/sites/default/files/2020-09/programming.jpg" /> </div>
+                 <div style={{color:"white", fontSize:"22px"}} > Mount Doodles Fam <br/>
+                 Website </div>
                  
                  </div>
                  <div className="myCards">
-                 <div style={{height:"50%"}} ><img style={{width:"100%", borderRadius:"10px 10px 0px 0px"}}src="https://www.gcu.edu/sites/default/files/2020-09/programming.jpg" /></div>
+                 <div style={{height:"100%", opacity:".7"}} ><img style={{height:"100%", borderRadius:"10px 10px 0px 0px"}}src="https://www.gcu.edu/sites/default/files/2020-09/programming.jpg" /></div>
                  <div > Text Information Goes there for me if i want to do text</div>
                  
                  </div>
@@ -28,12 +30,12 @@ export default function Home(){
 
                  <div className="cardsHouse">
                  <div className="myCards">
-                 <div style={{height:"50%"}} ><img style={{width:"100%", borderRadius:"10px 10px 0px 0px"}}src="https://www.gcu.edu/sites/default/files/2020-09/programming.jpg" /></div>
+                 <div style={{height:"100%", opacity:".8"}} ><img style={{height:"100%", borderRadius:"10px 10px 0px 0px"}}src="https://www.gcu.edu/sites/default/files/2020-09/programming.jpg" /></div>
                  <div > Text Information Goes there for me if i want to do text</div>
                  
                  </div>
                  <div className="myCards">
-                 <div style={{height:"50%"}} ><img style={{width:"100%", borderRadius:"10px 10px 0px 0px"}}src="https://www.gcu.edu/sites/default/files/2020-09/programming.jpg" /></div>
+                 <div style={{height:"100%", opacity:".8"}} ><img style={{height:"100%", borderRadius:"10px 10px 0px 0px"}}src="https://www.gcu.edu/sites/default/files/2020-09/programming.jpg" /></div>
                  <div > Text Information Goes there for me if i want to do text</div>
                  
                 </div>
